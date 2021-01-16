f7fd71cb559cda388171bec1dffc98a46ef8ebba
Jindřich Máca <tunik93@gmail.com>
2021-01-16 02:49:26 +0100

    Add helper function for rendering components with theme in tests

---
+++ src/testing/renderWithTheme.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import { DefaultTheme, ThemeProvider } from 'styled-components'
+import { DarkTheme } from '../themes'
+
+type RenderFunction = typeof render
+
+const renderWithTheme = (
+	ui: Parameters<RenderFunction>[0],
+	options?: Parameters<RenderFunction>[1] & { theme?: DefaultTheme }
+): ReturnType<RenderFunction> =>
+	render(
+		<ThemeProvider theme={options?.theme ?? DarkTheme}>{ui}</ThemeProvider>
+	)
+
+export default renderWithTheme
