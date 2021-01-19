d7bd9671bf7b8550ad68777d45657327cb7af586
Jindřich Máca <tunik93@gmail.com>
2021-01-19 22:53:16 +0100

    Improve grid elements layout styles

--- src/components/Header/styled.ts
+++ src/components/Header/styled.ts
@@ -1,27 +1,19 @@
 import styled from 'styled-components'
-import Toggle from '../Toggle'
 import Svg from '../../assets/header.svg'
 
 export const Wrapper = styled.header`
-	align-content: start;
 	background-image: url(${Svg});
 	background-repeat: no-repeat;
 	background-size: cover;
 	display: grid;
 	grid-template-columns: 1fr 3fr 1fr;
+	justify-items: center;
+	align-content: start;
 	height: 45em;
 `
 
 export const Title = styled.h1`
-	text-align: center;
 	font-size: 3em;
 	grid-column: 2;
-	justify-self: center;
 	margin: 1em;
-`
-
-// TOGGLE
-export const StyledToggle = styled(Toggle)`
-	justify-self: end;
-	margin: 0 4em 0 0;
 `
