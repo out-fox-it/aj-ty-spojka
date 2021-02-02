6799639a4eb3717a5c5aa76c4cf2f4dd18498a28
agnes97 <janachaloupkova97@gmail.com>
2021-02-02 09:44:01 +0100

    Add Registration page
    Add styled registration form
    Handle onSubmit with console.log
    Add hiding PasswordMessage logic onFocus and onBlur
    Add react-hook-form library validations with errors

--- src/components/RegisterForm/styled.ts
+++ src/components/RegisterForm/styled.ts
@@ -1,3 +1,96 @@
 import styled from 'styled-components'
+import { withAlphaHex } from 'with-alpha-hex'
 
-export const Wrapper = styled.section``
+export const Form = styled.form`
+	display: flex;
+	flex-flow: column wrap;
+	align-content: center;
+	margin: 2em;
+`
+
+export const FormItem = styled.div`
+	display: flex;
+	flex-flow: row nowrap;
+	align-items: center;
+	justify-content: space-between;
+`
+
+export const FormInput = styled.input`
+	margin: 1em;
+	padding: 0.7em;
+
+	&:focus {
+		outline: none;
+	}
+`
+
+export const FormCheckbox = styled.input`
+	margin: 1.5em;
+	padding: 0.5em;
+	transform: scale(1.5);
+`
+
+export const FormButton = styled.button`
+	margin: 1em;
+	padding: 0.5em;
+	flex-grow: 1;
+
+	color: ${({ theme }) => theme.text.secondary};
+	background-color: ${({ theme }) =>
+		withAlphaHex(theme.bg.buttonBackground, 0.5)};
+	cursor: pointer;
+
+	border: 0.1em solid ${({ theme }) => withAlphaHex(theme.bg.secondary, 0.5)};
+	border-radius: 0.8em;
+
+	&:hover {
+		color: ${({ theme }) => theme.bg.primary};
+		background-color: ${({ theme }) =>
+			withAlphaHex(theme.text.secondary, 0.7)};
+	}
+`
+
+export const Label = styled.label`
+	margin: 1em;
+`
+
+export const PasswordMessage = styled.div`
+	display: ${(props) => (props?.hidden ? 'none' : 'grid')};
+	grid: repeat(2, 1fr) / repeat(2, 1fr);
+	justify-items: center;
+	gap: 1em;
+	margin: 1em;
+`
+
+export const Errors = styled.span`
+	margin: 0;
+	text-align: center;
+	color: ${({ theme }) => theme.text.false};
+`
+
+export const HiddenError = styled.span`
+	display: none;
+	margin: 0;
+`
+
+export const ValidPassword = styled.p`
+	margin: 0;
+	color: ${({ theme }) => theme.text.true};
+
+	&:before {
+		position: relative;
+		left: -0.5em;
+		content: '✔';
+	}
+`
+
+export const InvalidPassword = styled.p`
+	margin: 0;
+	color: ${({ theme }) => theme.text.false};
+
+	&:before {
+		position: relative;
+		left: -0.5em;
+		content: '✖';
+	}
+`
