3c026343fb34d0c675c7f5a42c458434b5d6e247
Martina Vilimova <vilima.tul@gmail.com>
2021-02-10 15:21:34 +0100

    Functional modification of data in Name component

--- src/components/ContentProfile/index.tsx
+++ src/components/ContentProfile/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
+import { Name } from './Name'
 import {
 	Title,
 	Center,
-	Name,
 	SocialNetwork,
 	TextArea,
 	StyledButton,
@@ -15,36 +15,40 @@ interface Props {
 	email: string
 }
 
-const ContentProfile: React.FC<Props> = ({ fullName, email }) => (
-	<Wrapper>
-		<Center>
-			<Name>{fullName}</Name>
-			<SocialNetwork>{email}</SocialNetwork>
-			<Space />
-			<Title>Něco o mě</Title>
-			<TextArea>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem
-				quam pariatur delectus eaque. Enim in adipisci quidem facere
-				doloremque, ullam eum aperiam totam, quia eveniet vel vitae
-				accusamus libero officiis. Lorem ipsum dolor sit, amet
-				consectetur adipisicing elit. Asperiores eaque perspiciatis
-				minus nesciunt dolor quaerat odit suscipit architecto qui
-				dolorem, id provident sapiente molestiae impedit magnam adipisci
-				nam illo blanditiis? Lorem ipsum dolor sit, amet consectetur
-				adipisicing elit. Nulla magnam ducimus blanditiis nam, alias
-				nobis non corporis necessitatibus quia saepe repudiandae
-				distinctio recusandae perspiciatis sit quasi minus mollitia
-				cumque facilis.
-			</TextArea>
-			<Space />
-			<Title>Kde mě najdeš</Title>
-			<SocialNetwork>www.seznam.cz</SocialNetwork>
-			<SocialNetwork>www.facebook.com</SocialNetwork>
-			<Space />
-			<StyledButton title="Odhlásit se" />
-			<Space />
-		</Center>
-	</Wrapper>
-)
+const ContentProfile: React.FC<Props> = ({ fullName, email }) => {
+	return (
+		<Wrapper>
+			<Center>
+				<Name fullName={fullName} />
+				<SocialNetwork>{email}</SocialNetwork>
+				<Space />
+				<Title>Něco o mě</Title>
+				<TextArea>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit.
+					Quidem quam pariatur delectus eaque. Enim in adipisci quidem
+					facere doloremque, ullam eum aperiam totam, quia eveniet vel
+					vitae accusamus libero officiis. Lorem ipsum dolor sit, amet
+					consectetur adipisicing elit. Asperiores eaque perspiciatis
+					minus nesciunt dolor quaerat odit suscipit architecto qui
+					dolorem, id provident sapiente molestiae impedit magnam
+					adipisci nam illo blanditiis? Lorem ipsum dolor sit, amet
+					consectetur adipisicing elit. Nulla magnam ducimus
+					blanditiis nam, alias nobis non corporis necessitatibus quia
+					saepe repudiandae distinctio recusandae perspiciatis sit
+					quasi minus mollitia cumque facilis.
+				</TextArea>
+				<Space />
+				<Title>Kde mě najdeš</Title>
+				<SocialNetwork>www.seznam.cz</SocialNetwork>
+				<SocialNetwork>www.facebook.com</SocialNetwork>
+				<Space />
+				<StyledButton title="Odhlásit se" />
+				<Space />
+				<StyledButton title="Přidat kontakt" />
+				<Space />
+			</Center>
+		</Wrapper>
+	)
+}
 
 export default ContentProfile
