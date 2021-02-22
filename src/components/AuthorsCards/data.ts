26b2d52d9c37432969afdb31373e27c224306173
agnes97 <janachaloupkova97@gmail.com>
2021-02-22 21:03:21 +0100

    Refactor component data as separate file and props

---
+++ src/components/AuthorsCards/data.ts
@@ -0,0 +1,61 @@
+// AuthorsCards images
+import jindrichmaca from '../../assets/AuthorsCards/jindrichmaca.jpg'
+import janachaloupkova from '../../assets/AuthorsCards/janachaloupkova.jpg'
+import martinavilimova from '../../assets/AuthorsCards/martinavilimova.jpeg'
+import tomashampl from '../../assets/AuthorsCards/tomashampl.jpg'
+
+type CardData = {
+	cardTitle: string
+	cardSubtitle: string
+	image: string
+
+	socialMediaLinks: {
+		gitHub: string
+		linkedIn: string
+		instagram?: string
+	}
+}
+
+export const cardsData: Readonly<CardData[]> = [
+	{
+		cardTitle: 'Jindřich Máca',
+		cardSubtitle: '~ full-stack, functional programming addict ~',
+		image: jindrichmaca,
+
+		socialMediaLinks: {
+			gitHub: 'https://github.com/macajind',
+			linkedIn: 'https://www.linkedin.com/in/macajind/',
+		},
+	},
+	{
+		cardTitle: 'Jana Chaloupková',
+		cardSubtitle: '~ front-end, Typescript, React, Clojurian ~',
+		image: janachaloupkova,
+
+		socialMediaLinks: {
+			gitHub: 'https://github.com/agnes97',
+			linkedIn: 'https://www.linkedin.com/in/agnes97',
+			instagram: 'https://www.linkedin.com/in/agnes97',
+		},
+	},
+	{
+		cardTitle: 'Martina Vilímová',
+		cardSubtitle: '~ front-end, TypeScript, React ~',
+		image: martinavilimova,
+
+		socialMediaLinks: {
+			gitHub: 'https://github.com/MartinaVilimova',
+			linkedIn: 'https://www.linkedin.com/in/martinavilimova/',
+		},
+	},
+	{
+		cardTitle: 'Tomáš Hampl',
+		cardSubtitle: '~ back-end, TypeScript, React ~',
+		image: tomashampl,
+
+		socialMediaLinks: {
+			gitHub: 'https://github.com/tomas90hampl',
+			linkedIn: 'https://www.linkedin.com/in/tomáš-hampl-300b461b9/',
+		},
+	},
+]
