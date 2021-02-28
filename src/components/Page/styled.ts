6b10ff69208f4534f41bf99fae9bea0121e22436
Martina Vilimova <vilima.tul@gmail.com>
2021-02-28 17:17:47 +0100

    Fix Footer position

--- src/components/Page/styled.ts
+++ src/components/Page/styled.ts
@@ -6,12 +6,15 @@ export const GlobalWrapper = styled.article`
 	flex-flow: column nowrap;
 	margin: 0 auto;
 	max-width: 50%;
+	min-height: 66vh;
 
 	@media (max-width: ${MediaQueries.Ipad}) {
 		max-width: 75%;
+		min-height: 50vh;
 	}
 
 	@media (max-width: ${MediaQueries.Mobile}) {
 		max-width: 95%;
+		min-height: 50vh;
 	}
 `
