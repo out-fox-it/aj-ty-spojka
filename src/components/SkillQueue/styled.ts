e4590a79348d175ca4ec16eb3c0e3b9ce9b598ca
agnes97 <janachaloupkova97@gmail.com>
2021-03-15 21:37:42 +0100

    Add SkillQueue component and it's basic styles

---
+++ src/components/SkillQueue/styled.ts
@@ -0,0 +1,84 @@
+import styled from 'styled-components'
+import { FontSizes } from '../../themes'
+
+type LabelProps = {
+	labelText: string
+}
+
+export const Row = styled.div`
+	display: flex;
+	flex-flow: row;
+	gap: 1em;
+	margin-top: 1em;
+	margin-bottom: 1em;
+`
+
+export const MySkills = styled.div`
+	margin-top: -1.5em;
+`
+
+export const Label = styled.div<LabelProps>`
+	font-size: ${FontSizes.SizeDefault};
+	font-weight: bold;
+	color: ${({ theme }) => theme.text.secondary};
+	margin-top: auto;
+	margin-bottom: auto;
+`
+
+export const ChosenSkills = styled.div``
+
+export const AllSkillsBox = styled.div`
+	width: 100%;
+	height: 15em;
+	background-color: ${({ theme }) => theme.bg.secondary};
+	border-radius: 0.8em;
+	text-align: center;
+	overflow: auto;
+	border: 1.7em solid ${({ theme }) => theme.bg.secondary};
+
+	// Scrollbar styles
+	&::-webkit-scrollbar {
+		width: 0.5em;
+	}
+
+	/* Track */
+	&::-webkit-scrollbar-track {
+		background: ${({ theme }) => theme.bg.secondary};
+	}
+
+	/* Handle */
+	&::-webkit-scrollbar-thumb {
+		background: ${({ theme }) => theme.text.secondary};
+		border-radius: 0.8em;
+	}
+
+	/* Handle on hover */
+	&::-webkit-scrollbar-thumb:hover {
+		cursor: pointer;
+	}
+`
+
+export const Skill = styled.button`
+	font-size: ${FontSizes.SizeDefault};
+	padding: 0.5em;
+	margin: 0.3em;
+	background-color: ${({ theme }) => theme.bg.primary};
+	color: ${({ theme }) => theme.text.primary};
+	border: 1px solid ${({ theme }) => theme.text.link};
+	border-radius: 0.8em;
+	outline: none;
+	cursor: pointer;
+`
+export const SearchBoxContainer = styled.div`
+	padding-bottom: 1em;
+`
+
+export const SearchBox = styled.input`
+	font-size: ${FontSizes.SizeDefault};
+	padding: 0.5em;
+	margin: 0.5em;
+	width: 50%;
+	border-radius: 0.8em;
+	outline: none;
+	border: 0.2em solid ${({ theme }) => theme.text.link};
+`
