89209c6ac14e5a7c98b211415cf4f8160eef1185
agnes97 <janachaloupkova97@gmail.com>
2021-03-18 14:11:53 +0100

    Add registration data to Profile

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from 'react'
-import { getProfileData, RandomUser } from '../utils/getUserData'
+import React from 'react'
 
 import NavBar from '../components/NavBar'
 import Page from '../components/Page'
@@ -8,18 +7,9 @@ import Footer from '../components/Footer'
 import { useUser } from '../components/User'
 
 const Profile: React.FC = () => {
-	const [userData, setUserData] = useState<RandomUser | null>(null)
 	const { user } = useUser()
 
-	useEffect(() => {
-		getProfileData().then(setUserData)
-	}, [])
-
-	const title = userData?.name?.title ?? ''
-	const first = userData?.name?.first ?? ''
-	const last = userData?.name?.last ?? ''
-	const fullName = `${title} ${first} ${last}`
-
+	const fullName = user?.displayName ?? ''
 	const email = user?.email ?? ''
 
 	return (
