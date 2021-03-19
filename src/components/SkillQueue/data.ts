e99da39955c64b0424eee00e0f04a82a7fa506fa
agnes97 <janachaloupkova97@gmail.com>
2021-03-19 21:30:20 +0100

    Add and map data for SkillQueue

---
+++ src/components/SkillQueue/data.ts
@@ -0,0 +1,243 @@
+type SkillsTypes = {
+	skillTitle: string
+	skillType: string
+	skillAlias: string[]
+}
+
+export const skillsData: Readonly<SkillsTypes[]> = [
+	{
+		skillTitle: 'JavaScript',
+		skillType: 'language',
+		skillAlias: ['JS'],
+	},
+	{
+		skillTitle: 'TypeScript',
+		skillType: 'language',
+		skillAlias: ['TS'],
+	},
+	{
+		skillTitle: 'React',
+		skillType: 'library',
+		skillAlias: ['React.js'],
+	},
+	{
+		skillTitle: 'Clojure',
+		skillType: 'language',
+		skillAlias: [''],
+	},
+	{
+		skillTitle: 'ClojureScript',
+		skillType: 'language',
+		skillAlias: [''],
+	},
+	{
+		skillTitle: 'Haskell',
+		skillType: 'language',
+		skillAlias: [''],
+	},
+	{
+		skillTitle: 'Java',
+		skillType: 'language',
+		skillAlias: [''],
+	},
+	{
+		skillTitle: 'C#',
+		skillType: 'language',
+		skillAlias: [''],
+	},
+	{
+		skillTitle: 'PHP',
+		skillType: 'language',
+		skillAlias: [''],
+	},
+	{
+		skillTitle: 'Vue',
+		skillType: 'framework',
+		skillAlias: ['Vue.js'],
+	},
+	{
+		skillTitle: 'Nette',
+		skillType: 'framework',
+		skillAlias: [''],
+	},
+	{
+		skillTitle: 'Python',
+		skillType: 'language',
+		skillAlias: [''],
+	},
+	{
+		skillTitle: 'Scala',
+		skillType: 'language',
+		skillAlias: [''],
+	},
+	{
+		skillTitle: 'Swift',
+		skillType: 'language',
+		skillAlias: [''],
+	},
+	{
+		skillTitle: 'Go',
+		skillType: 'language',
+		skillAlias: [''],
+	},
+	{
+		skillTitle: 'Elm',
+		skillType: 'language',
+		skillAlias: [''],
+	},
+	{
+		skillTitle: 'Ruby',
+		skillType: 'language',
+		skillAlias: [''],
+	},
+	{
+		skillTitle: 'Rust',
+		skillType: 'language',
+		skillAlias: [''],
+	},
+	{
+		skillTitle: 'Scala',
+		skillType: 'language',
+		skillAlias: [''],
+	},
+	{
+		skillTitle: 'Angular',
+		skillType: 'language',
+		skillAlias: ['Angular.js'],
+	},
+	{
+		skillTitle: 'jQuery',
+		skillType: 'framework',
+		skillAlias: [''],
+	},
+	{
+		skillTitle: 'Django',
+		skillType: 'framework',
+		skillAlias: [''],
+	},
+	{
+		skillTitle: 'Java',
+		skillType: 'language',
+		skillAlias: [''],
+	},
+	{
+		skillTitle: 'C',
+		skillType: 'language',
+		skillAlias: [''],
+	},
+	{
+		skillTitle: 'Perl',
+		skillType: 'language',
+		skillAlias: [''],
+	},
+	{
+		skillTitle: 'C++',
+		skillType: 'language',
+		skillAlias: [''],
+	},
+	{
+		skillTitle: 'R',
+		skillType: 'language',
+		skillAlias: [''],
+	},
+	{
+		skillTitle: 'Arduino',
+		skillType: 'language',
+		skillAlias: [''],
+	},
+	{
+		skillTitle: 'Groovy',
+		skillType: 'language',
+		skillAlias: [''],
+	},
+	{
+		skillTitle: 'Delphi',
+		skillType: 'language',
+		skillAlias: [''],
+	},
+	{
+		skillTitle: 'Pascal',
+		skillType: 'language',
+		skillAlias: [''],
+	},
+	{
+		skillTitle: 'D',
+		skillType: 'language',
+		skillAlias: [''],
+	},
+	{
+		skillTitle: 'WebAssembly',
+		skillType: 'language',
+		skillAlias: [''],
+	},
+	{
+		skillTitle: 'Swift',
+		skillType: 'language',
+		skillAlias: [''],
+	},
+	{
+		skillTitle: 'F#',
+		skillType: 'language',
+		skillAlias: [''],
+	},
+	{
+		skillTitle: 'Erlang',
+		skillType: 'language',
+		skillAlias: [''],
+	},
+	{
+		skillTitle: 'HTML/CSS',
+		skillType: 'language',
+		skillAlias: [''],
+	},
+	{
+		skillTitle: 'Node.js',
+		skillType: 'framework',
+		skillAlias: [''],
+	},
+	{
+		skillTitle: '.Net',
+		skillType: 'framework',
+		skillAlias: [''],
+	},
+	{
+		skillTitle: 'Kotlin',
+		skillType: 'language',
+		skillAlias: [''],
+	},
+	{
+		skillTitle: 'Laravel',
+		skillType: 'framework',
+		skillAlias: [''],
+	},
+	{
+		skillTitle: 'Flask',
+		skillType: 'framework',
+		skillAlias: [''],
+	},
+	{
+		skillTitle: 'Julia',
+		skillType: 'language',
+		skillAlias: [''],
+	},
+	{
+		skillTitle: 'Symfony',
+		skillType: 'framework',
+		skillAlias: [''],
+	},
+	{
+		skillTitle: 'Gatsby',
+		skillType: 'framework',
+		skillAlias: [''],
+	},
+	{
+		skillTitle: 'Drupal',
+		skillType: 'framework',
+		skillAlias: [''],
+	},
+	{
+		skillTitle: 'ASP.NET',
+		skillType: 'language',
+		skillAlias: [''],
+	},
+]
